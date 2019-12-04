6e59180557e33dbb9bb45e08cd5c904f03e48b6e
Anthony Hanson <ahanson@udallas.edu>
2019-12-03 17:06:25 -0800

    make book table

---
+++ Project3Sample/BookCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookCell.swift
+//  Project3Sample
+//
+//  Created by Anthony Hanson on 12/3/19.
+//  Copyright © 2019 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+    
+    @IBOutlet weak var bookTitle: UILabel!
+    
+}
--- Project3Sample/BookListViewController.swift
+++ Project3Sample/BookListViewController.swift
@@ -8,9 +8,47 @@
 
 import UIKit
 
+import UIKit
+
 class BookListViewController: UIViewController {
     
+    @IBOutlet weak var bookTable: UITableView!
     
+    let bookService = BookService.shared
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bookService.fetchBooks {
+            DispatchQueue.main.async {
+                self.bookTable.reloadData()
+            }
+        }
+        
+        bookTable.dataSource = self
+        bookTable.delegate = self
+    }
+}
+
+extension BookListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (bookService.books.count)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let book = bookService.books[indexPath.item]
+        let title = book.title
+        
+        let cell = bookTable.dequeueReusableCell(withIdentifier: "BookCell")!
+        if let bookCell = cell as? BookCell {
+            bookCell.bookTitle.text = title
+            return bookCell
+        }
+        
+        return cell
+    }
+
+
+}
+extension BookListViewController: UITableViewDelegate {
     
 }
